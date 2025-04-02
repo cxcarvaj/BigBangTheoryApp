fcab95c3085b9df83ca13663bbf0876de253fbaa
cxcarvaj <cxcarvaj@gmail.com>
2025-04-01 20:04:07 -0500

    Initial Commit

---
+++ BigBangTheory/BigBangTheoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBangTheoryApp.swift
+//  BigBangTheory
+//
+//  Created by Carlos Xavier Carvajal Villegas on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BigBangTheoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
